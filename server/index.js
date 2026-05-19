f6ac0f62ea24524bfcae9fa90ff87a4124643227
saransh1317 <saranshchaudhary713@gmail.com>
2026-05-19 12:20:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import * as dns from "node:dns";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
