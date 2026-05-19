57e9b95916d743c627878f329e95db5543cd3a2c
saransh1317 <saranshchaudhary713@gmail.com>
2026-05-19 12:01:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,18 +17,33 @@ app.use("/", router);
 app.get("/",(req,res)=>{
   res.send("Backend is working perfectly")
 })
+// const mongoose = require('mongoose');
+// Start app after connecting to MongoDB
 mongoose.set('strictQuery', true);
 dns.setServers(['8.8.8.8', '1.1.1.1']);
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-}).catch((err) => {
-  console.log({ err });
-});
-const server = app.listen(2500, () => {
-  const address = server.address();
-  if (address && typeof address === 'object') {
-    console.log(`Server is listening at http://${address.address === '::' ? 'localhost' : address.address}:${address.port}`);
-  } else {
-    console.log(`Server is listening`);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log('Mongodb connected');
+
+    const port = process.env.PORT;
+    const server = app.listen(port, () => {
+      const address = server.address();
+      if (address && typeof address === 'object') {
+        console.log(`Server is listening at http://${address.address === '::' ? 'localhost' : address.address}:${address.port}`);
+      } else {
+        console.log(`Server is listening`);
+      }
+    });
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
   }
-});
+};
+
+startServer();
